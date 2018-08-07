50769bb4aad513d7c7dfe2da6d1662f4b93346a8
liubingwen <liubingwen@huianrong.com>
2018-08-07 13:47:08 +0800

    修改二维码生成代码

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,134 +1,119 @@
 <template>
   <div>
-    <canvas ref="canvas" :width="size" :height="size"></canvas>
+    <canvas :height="size" :width="size" ref="qrcode-vue" ></canvas>
   </div>
 </template>
 <script>
-  /* eslint-disable promise/param-names */
   import QRCode from 'qr.js/lib/QRCode'
   import ErrorCorrectLevel from 'qr.js/lib/ErrorCorrectLevel'
+
+  function getBackingStorePixelRatio (ctx) {
+    return (
+      ctx.webkitBackingStorePixelRatio ||
+      ctx.mozBackingStorePixelRatio ||
+      ctx.msBackingStorePixelRatio ||
+      ctx.oBackingStorePixelRatio ||
+      ctx.backingStorePixelRatio ||
+      1
+    )
+  }
+
+  function detectZoom () {
+    let ratio = 0
+    const screen = window.screen
+    const ua = navigator.userAgent.toLowerCase()
+
+    if (window.devicePixelRatio !== undefined) {
+      ratio = window.devicePixelRatio
+    } else if (~ua.indexOf('msie')) {
+      if (screen.deviceXDPI && screen.logicalXDPI) {
+        ratio = screen.deviceXDPI / screen.logicalXDPI
+      }
+    } else if (window.outerWidth !== undefined && window.innerWidth !== undefined) {
+      ratio = window.outerWidth / window.innerWidth
+    }
+
+    if (ratio) {
+      ratio = Math.round(ratio * 100)
+    }
+    return ratio / 100
+  }
+
   export default {
-    name: 'harQrcode',
     props: {
-      // 要编码的字符串
-      text: {
+      value: {
         type: String,
         required: true,
         default: ''
       },
-      /*
-       * 二维码的宽和高，单位是px，只允许生成正方形二维码
-       * 需要注意的是，当使用table绘制二维码时，会适当减小，使得能够整除二维码矩阵的维度。
-       */
       size: {
-        type: [Number, String],
-        default: 256,
+        type: Number | String,
+        default: 100,
         validator: s => isNaN(Number(s)) !== true
       },
-      // 纠错级别，可取0、1、2、3，数字越大说明所需纠错级别越大
       level: {
         type: String,
         default: 'H',
-        validator: l => ['L', 'Q', 'M', 'H'].indexOf(l) > -1
+        validator: l => ['L', 'M', 'Q', 'H'].indexOf(l) > -1
       },
-      // 背景色
       background: {
         type: String,
         default: '#fff'
       },
-      // 前背景色
       foreground: {
         type: String,
         default: '#000'
       },
-      // 码正中间图片的url，只支持配置正方形图片
-      image: {
-        type: String
-      },
-      // image的宽和高
-      imageSize: {
-        type: Number,
-        default: 80
-      }
-    },
-    data () {
-      return {
+      logo: {
+        type: String,
+        default: ''
       }
     },
     watch: {
-      text () {
+      value (e) {
+        // 修复url修改时数据更新
         this.render()
       }
     },
     methods: {
-      getPixelRatio (context) {
-        const backingStore = context.backingStorePixelRatio ||
-          context.webkitBackingStorePixelRatio ||
-          context.mozBackingStorePixelRatio ||
-          context.msBackingStorePixelRatio ||
-          context.oBackingStorePixelRatio ||
-          context.backingStorePixelRatio ||
-          1
-        return (window.devicePixelRatio || 1) / backingStore
-      },
-      loadImg (url) {
-        return new Promise((reslove, reject) => {
-          const img = new Image()
-          img.onload = function () {
-            reslove(this)
-            img.onload = null
-          }
-          img.src = url
-        })
-      },
-      /**
-       * 计算矩阵点的前景色
-       * @param {Obj} config
-       * @param {Number} row 点x坐标
-       * @param {Number} col 点y坐标
-       * @param {Number} count 矩阵大小
-       * @return {String}
-       */
-      getForeGround ({row, col, count}) {
-        const {pdground, foreground} = this
-        const temp1 = row > 1 && row < 5 && col > 1 && col < 5
-        const temp2 = row > count - 6 && row < count - 2 && col > 1 && col < 5
-        const temp3 = row > 1 && row < 5 && col > count - 6 && col < count - 2
-        if (pdground && (temp1 || temp2 || temp3)) {
-          return pdground
-        }
-        return foreground
-      },
       async render () {
-        const {size, image, imageSize, background, foreground, level, text} = this
+        const {value, size, level, background, foreground} = this
+        const _size = size >>> 0 // size to number
         // We'll use type===-1 to force QRCode to automatically pick the best type
         const qrCode = new QRCode(-1, ErrorCorrectLevel[level])
-        qrCode.addData(text)
+        qrCode.addData(value)
         qrCode.make()
-        const canvas = this.$refs.canvas
+
+        const canvas = this.$refs['qrcode-vue']
+
         const ctx = canvas.getContext('2d')
         const cells = qrCode.modules
-        const ratio = 2 // this.getPixelRatio(ctx)
-        const ratioSize = ratio * size
-        const tileSize = (ratioSize / cells.length).toPrecision(4)
-        const ratioImgSize = imageSize * ratio
-        console.log(ratio)
-        ctx.scale(1 / ratio, 1 / ratio)
-        // 绘制
-        cells.forEach((row, rdx) => {
-          row.forEach((cell, cdx) => {
+        const tileW = _size / cells.length
+        const tileH = _size / cells.length
+        canvas.height = canvas.width = _size
+        ctx.scale(1, 1)
+        console.log(detectZoom())
+        cells.forEach(function (row, rdx) {
+          row.forEach(function (cell, cdx) {
             ctx.fillStyle = cell ? foreground : background
-            const w = (Math.ceil((cdx + 1) * tileSize) - Math.floor(cdx * tileSize))
-            const h = (Math.ceil((rdx + 1) * tileSize) - Math.floor(rdx * tileSize))
-            ctx.fillRect(Math.round(cdx * tileSize), Math.round(rdx * tileSize), w, h)
+            const w = (Math.ceil((cdx + 1) * tileW) - Math.floor(cdx * tileW))
+            const h = (Math.ceil((rdx + 1) * tileH) - Math.floor(rdx * tileH))
+            ctx.fillRect(Math.round(cdx * tileW), Math.round(rdx * tileH), w, h)
           })
         })
-        if (image) {
-          const img = await this.loadImg(image)
-          const x = ((ratioSize - ratioImgSize) / 2).toFixed(2)
-          const y = x
-          ctx.drawImage(img, x, y, ratioImgSize, ratioImgSize)
+        if (this.logo) {
+          const img = await this.imgLoad(this.logo)
+          ctx.drawImage(img, _size / 2 - img.width / 2, _size / 2 - img.height / 2)
         }
+      },
+      imgLoad (url) {
+        return new Promise((resolve, reject) => {
+          const imgObj = new Image()
+          imgObj.onload = function () {
+            resolve(imgObj)
+          }
+          imgObj.src = url
+        })
       },
       _fixType (type) {
         type = type.toLowerCase().replace(/jpg/i, 'jpeg')
@@ -153,7 +138,7 @@
     updated () {
       this.render()
     },
-    mounted () {
+    async mounted () {
       this.render()
     }
   }
